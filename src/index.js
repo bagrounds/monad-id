346df0014584b3ad88792089c9c677abb32ec537
bagrounds <bagrounds@gmail.com>
2017-04-03 19:43:01 -0700

    [MINOR][CHANGE] use latest version of fun-monad

--- src/index.js
+++ src/index.js
@@ -7,37 +7,18 @@
 
   /* imports */
   var funMonad = require('fun-monad')
+  var id = require('fun-id')
 
   /* exports */
   module.exports = funMonad({
-    type: Id,
-    of: Id,
+    type: id,
+    of: id,
     map: map,
-    join: join
+    join: id
   })
 
-  /**
-   *
-   * @function module:monad-id.Id
-   *
-   * @param {*} value - anything
-   *
-   * @return {Id} a new instance of Id containing value
-   */
-  function Id (value) {
-    if (!this) {
-      return new Id(value)
-    }
-
-    this.value = value
-  }
-
-  function join (mma) {
-    return mma.value
-  }
-
   function map (f, ma) {
-    return Id(f(ma.value))
+    return f(ma)
   }
 })()
 
